11f3b727d6101bb072cbea257aafa79812392227
matthewgreer <matthew.greer.dev@gmail.com>
2024-02-12 18:47:06 -0500

    create & run routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  namespace :api, defaults: { format: :json } do
+    
+    resources :users, only: [:create, :update] do
+      get 'user_dailies/:wordcross_date', to: 'user_dailies#show', as: 'daily_fetch'
+      patch 'user_dailies/:id', to: 'user_dailies#update', as: 'daily_update'
+      get 'user_micros/:wordcross_date', to: 'user_micros#show', as: 'micro_fetch'
+      patch 'user_micros/:id', to: 'user_micros#update', as: 'micro_update'
+    end
+
+    get 'dailies/:wordcross_date', to: 'dailies#show', as: 'daily_fetch_author'
+    get 'micros/:wordcross_date', to: 'micros#show', as: 'micro_fetch_author'
+
+    resource :session, only: [:create, :destroy]
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
