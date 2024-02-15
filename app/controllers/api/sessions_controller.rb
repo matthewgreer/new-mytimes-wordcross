ca7c58948d0906045e8d6762ccb9cfe0040c7c14
matthewgreer <matthew.greer.dev@gmail.com>
2024-02-14 21:54:20 -0500

    add sessions controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,22 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+
+    if @user
+      login!(@user)
+      render '/api/users/show'
+    else
+      # custom error messages - is there a better way to do this?
+      errors = []
+      errors << "Please enter your username or email address." if params[:user][:email] == ""
+      errors << "Please enter a password." if params[:user][:password] == ""
+      errors << "The email address and password you entered don't match any MYTimes account. Please try again." if errors.empty?
+      render json: { messages: errors, email: params[:user][:email] }, status: 422
+    end
+  end
+
+  def destroy
+    logout!
+    render json: ['']
+  end
+end
