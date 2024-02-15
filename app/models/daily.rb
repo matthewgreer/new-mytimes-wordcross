08a750f458eab950a7ecddf3ac0bcfd22a9e9d33
matthewgreer <matthew.greer.dev@gmail.com>
2024-02-14 22:00:40 -0500

    create daily & micro ctrlrs & models

---
+++ app/models/daily.rb
@@ -0,0 +1,9 @@
+class Daily < ApplicationController
+  validates :wordcross_date, presence: true, uniqueness: true
+  validates :author, presence: true
+  validates :solution, presence: true
+  validates :clue_set, presence: true
+  validates :label_set, presence: true
+
+  has_many :user_dailies
+end
